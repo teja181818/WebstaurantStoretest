178fd0936fb96f6429d8ecefd2f2b944e0ff815b
teja181818 <teja1718@gmail.com>
2024-03-30 12:12:40 -0500

    test for webstaurant
    test for webstaurant

---
+++ stainless-work-table/src/main/java/webstaurantstoretest/stainless_work_table/App.java
@@ -0,0 +1,13 @@
+package webstaurantstoretest.stainless_work_table;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
